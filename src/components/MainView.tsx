7ade6c42b8c4620f3174ed87c0b6f08d71ce91de
Juan Pablo Saucedo <juan.saucedo@mi.unc.edu.ar>
2022-04-25 11:57:00 -0300

    Added form to choose algorithm

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -10,16 +10,36 @@ interface BarI {
 
 const MainView = () => {
 
-    const [algorithm, setAlgorithm] = useState<"bubblesort" | "quicksort" | null>(null);
+    const [algorithm, setAlgorithm] = useState<string | null>(null);
 
+    const handleAlgorithmChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setAlgorithm(event.target.value);
+    }
 
+    
     return (
         <main>
-            <button onClick={(e) => setAlgorithm("bubblesort")}>Empezar</button>
+            <header>
+                <h1>Sorting Visualizer</h1>
+            </header>
+
+            <form onSubmit={(e) => { e.preventDefault() }}
+            className="d-flex flex-row p-4">
+                <div className="d-flex flex-column">
+                    <label>Choose an algorithm:</label>
+                    <select name="algorithm" id="cars" onChange={handleAlgorithmChange}>
+                        <option value="bubblesort">Bubblesort</option>
+                        <option value="quicksort">Quicksort</option>
+                    </select>
+                </div>
+
+                <div className="d-flex flex-column">
+                    <button onClick={(e) => setAlgorithm("bubblesort")}>Empezar</button>
+                    <button onClick={(e) => setAlgorithm("bubblesort")}>Shuffle</button>
+                </div>
+            </form>
             {algorithm === "bubblesort" && <BubbleSort />}
-
         </main>
-
     )
 }
 
